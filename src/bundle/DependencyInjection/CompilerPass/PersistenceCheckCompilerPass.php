66c356a31b071c6324c1c6088c57662e2261f55b
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2023-07-12 14:51:11 +0200

    Fixed code style

--- src/bundle/DependencyInjection/CompilerPass/PersistenceCheckCompilerPass.php
+++ src/bundle/DependencyInjection/CompilerPass/PersistenceCheckCompilerPass.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
 namespace Ibexa\Bundle\Test\Core\DependencyInjection\CompilerPass;
 
 use Ibexa\Contracts\CorePersistence\Gateway\AbstractDoctrineDatabase;
